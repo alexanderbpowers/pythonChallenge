8cfbc942cee788dbb0518e0c3dcbceb381da028c
alexanderbpowers <101031351+alexanderbpowers@users.noreply.github.com>
2022-05-27 17:11:41 +1000

    Homework finished

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,47 +3,38 @@ import csv
 
 csvpath= "/Users/alexanderpowers/Documents/GitHub/pythonChallenge/PyPoll/Resources/election_data.csv"
 
-total=[]
 candidates=[]
-
-
+totalVotes=0
+votes={}
+percent={}
+candString={}
+candStringList=[]
 
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile,delimiter=",")
     csv_header = next(csvreader)
 
-
-
     for row in csvreader:
+        totalVotes = totalVotes + 1
+        candidateName = row[2]
 
-        if any(row[1]):
-            total.append(str(row[1]))
-            totalVotes = len(total)
-        if str(row[2]):
-            candidates.append(str(row[2]))
-            candidatesSet=set(candidates)
-            candSetList=list(candidatesSet)
-        CandVotesAmount = {}
-        CandVotesPercent = {}
-        CandString = []
-print("hello")
-        for name in candidates:
-            CandVotesAmount[name]=candidates.count(name)
-            CandVotesPercent[name]=(CandVotesAmount[name]/totalVotes)*100
-        for name in candidatesSet:
-            CandString.append(f"{name}: {int(CandVotesPercent[name])}% ({CandVotesAmount[name]})")
+        if candidateName not in candidates:
+            candidates.append(candidateName)
+            votes[candidateName] = 0
+        votes[candidateName] = votes[candidateName] + 1
+        percent[candidateName] = (votes[candidateName] / totalVotes) * 100
+        candString[candidateName] = (f"{candidateName}: {int(percent[candidateName])}% ({votes[candidateName]})")
+        candStringList=candString.values()
 
-
-print("finished looping")
 textFile= open(r"/Users/alexanderpowers/Documents/GitHub/pythonChallenge/PyPoll/Analysis/analysis.txt", "w" )
 
 str1=("Election Results")
 str2=("-------------------------")
 str3=(f"Total Vote: {totalVotes}")
 str4=("-------------------------")
-strP='\n'.join(CandString)
+strP='\n'.join(candStringList)
 str5=("-------------------------")
-winner=max(CandVotesAmount,key=CandVotesAmount.get)
+winner=max(votes,key=votes.get)
 str6=(f"Winner: {winner}")
 str7=("-------------------------")
 
